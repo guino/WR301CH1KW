4b711e4d1c20056fdd42af7ed2dcf88c1dc5d9e8
wagner.oliveira <wagner.oliveira@fireking.com>
2020-11-12 22:07:01 -0500

    fixed cut-off nfs.sh script

--- nfs.sh
+++ nfs.sh
@@ -2,17 +2,17 @@
 
 # If we already have SD, leave
 if [ -e /mnt/mtd/ipc/tmpfs/sd/.sd_flag ]; then
-        exit 0
+	exit 0
 fi
 
 # Wait for reply from NFS server
 while [ 1 ]
 do
-        if ping -c 1 -W 1 10.10.10.2 > /dev/null
-        then
-                break
-        fi
-        sleep 5
+	if ping -c 1 -W 1 10.10.10.2 > /dev/null
+	then
+		break
+	fi
+	sleep 5
 done
 
 # Unpack files, mount them in /sys and touch flags to simulate SD
@@ -25,9 +25,9 @@ touch /mnt/mtd/ipc/tmpfs/sd_flag
 # Mount and Monitor NFS
 while [ 1 ]
 do
-        if [ ! -e /mnt/mtd/ipc/tmpfs/sd/.sd_flag ]; then
-                umount /mnt/mtd/ipc/tmpfs/sd 2> /dev/null
-                mount -o port=2049,nolock,proto=tcp -t nfs 10.10.10.2:/mnt/3TB/nvr-PTZ /mnt/mt
-        fi
-        sleep 15
+	if [ ! -e /mnt/mtd/ipc/tmpfs/sd/.sd_flag ]; then
+		umount /mnt/mtd/ipc/tmpfs/sd 2> /dev/null
+		mount -o port=2049,nolock,proto=tcp -t nfs 10.10.10.2:/mnt/3TB/nvr-PTZ /mnt/mtd/ipc/tmpfs/sd
+	fi
+	sleep 15
 done
